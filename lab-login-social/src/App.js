3a5455a757a623c0462ddb7fa60c124cac90c299
cleitonestefenon <cleitonestefenon@gmail.com>
2021-11-21 10:50:32 -0300

    primeiro commit login social

---
+++ lab-login-social/src/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Button } from '@material-ui/core';
+import Stack from '@mui/material/Stack';
+import './App.css';
+
+function App() {
+  return (
+    <Stack direction='row'>
+      <Button variant="contained">Contained</Button>
+    </Stack>
+  );
+}
+
+export default App;
