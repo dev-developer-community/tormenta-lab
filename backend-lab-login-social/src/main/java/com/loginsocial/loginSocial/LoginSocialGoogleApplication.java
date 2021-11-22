432f732a2732c7b42c3bd04cfc45c8fb70295bf1
cleitonestefenon <cleitonestefenon@gmail.com>
2021-11-21 21:37:52 -0300

    Criado backend para lab do login social

---
+++ backend-lab-login-social/src/main/java/com/loginsocial/loginSocial/LoginSocialGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.loginsocial.loginSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSocialGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSocialGoogleApplication.class, args);
+	}
+
+}
---
+++ backend-lab-login-social/src/test/java/com/loginsocial/loginSocial/LoginSocialGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginsocial.loginSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSocialGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
